58d72ff442a50e2b803d65d5e5cbe4dea09e626f
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-02-04 05:31:34 +0200

    main-section design

--- src/app/components/pages/land-page/home/home.component.ts
+++ src/app/components/pages/land-page/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { MainSectionComponent } from "../main-section/main-section.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MainSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
