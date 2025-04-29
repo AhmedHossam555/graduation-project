3d7689425705668988d0e4c9eb0e3b9a41cdbc34
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-04-30 02:20:43 +0300

    sidebar html, scss update

--- src/app/pages/main-dashboard/main-dashboard.component.ts
+++ src/app/pages/main-dashboard/main-dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {  RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "../../shared/layout/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-main-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './main-dashboard.component.html',
   styleUrl: './main-dashboard.component.scss'
 })
