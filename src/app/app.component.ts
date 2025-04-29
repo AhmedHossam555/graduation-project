e0d81c09aff725c07819369c30768c4c6a7fc68b
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-04-29 18:04:14 +0300

    install flowbite library component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlowbiteService } from './shared/services/flowbite/flowbite.service';
+import { initFlowbite } from 'flowbite';
+import { LoginComponent } from "./core/pages/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'graduation-project';
+  constructor(private _flowbiteService: FlowbiteService){
+  }
+  ngOnInit(): void {
+    this._flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
 }
