e812d33e87787c34d5fda64bc277d4f5c792432d
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-04-30 04:22:02 +0300

    header design for dashboard

--- src/app/pages/main-dashboard/main-dashboard.component.ts
+++ src/app/pages/main-dashboard/main-dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {  RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "../../shared/layout/sidebar/sidebar.component";
+import { HeaderComponent } from "../../shared/layout/header/header.component";
 
 @Component({
   selector: 'app-main-dashboard',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
   templateUrl: './main-dashboard.component.html',
   styleUrl: './main-dashboard.component.scss'
 })
