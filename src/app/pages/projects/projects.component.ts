cde8e84af8afae79ccfcb3ce7cf5c2aac0e5ebc3
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-01 05:24:46 +0300

    create project model component in ui component

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FlowbiteService } from '../../shared/services/flowbite/flowbite.service';
+import { initFlowbite } from 'flowbite';
+import { ProjectModalComponent } from '../../shared/components/ui/project-modal/project-modal.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [ProjectModalComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+      constructor(private _flowbiteService: FlowbiteService){
+        }
+        ngOnInit(): void {
+          this._flowbiteService.loadFlowbite((flowbite) => {
+            initFlowbite();
+          });
+        }
 }
