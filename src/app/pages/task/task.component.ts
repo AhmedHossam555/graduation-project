8d5ca59595d2c73341dd0fd621cf8954fb2e6257
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-02 09:07:29 +0300

    Add Task To In-Progress list ,   Cancel Task In Progress, Add Task To Done list, and   Cancel Task Done

--- src/app/pages/task/task.component.ts
+++ src/app/pages/task/task.component.ts
@@ -130,18 +130,18 @@ export class TaskComponent {
   onAddTaskToDo() {
     this.isVisibleToDo = !this.isVisibleToDo;
     console.log("Add Task To Do", this.taskValue);
-    // if (this.taskValue != null) {
-    //   this.tasks.push({
-    //     id: this.tasks.length + 1,
-    //     title: this.taskValue,
-    //     description: "New Task",
-    //     status: "To-do",
-    //     tags: [],
-    //     dueDate: "Jan 25",
-    //     priority: "Normal"
-    //   });
-    //   this.taskValue = null;
-    // }
+    if (this.taskValue != null) {
+      this.tasks.push({
+        id: this.tasks.length + 1,
+        title: this.taskValue,
+        description: "New Task",
+        status: "To-do",
+        tags: [],
+        dueDate: "Jan 25",
+        priority: "Normal"
+      });
+      this.taskValue = null;
+    }
   }
   // Cancel Task To Do
   onCancelTaskToDo() {
@@ -149,4 +149,58 @@ export class TaskComponent {
     this.taskValue = null;
   }
 
+  // Add Task To In-Progress list
+  isVisibleInProgress: boolean = false;
+  onAddTaskTwo() {
+    this.isVisibleInProgress = !this.isVisibleInProgress;
+    console.log("Add Task In Progress", this.taskValue);
+  }
+  onAddTaskInProgress() {
+    this.isVisibleInProgress = !this.isVisibleInProgress;
+    console.log("Add Task In Progress", this.taskValue);
+    if (this.taskValue != null) {
+      this.tasks.push({
+        id: this.tasks.length + 1,
+        title: this.taskValue,
+        description: "New Task",
+        status: "In-progress",
+        tags: [],
+        dueDate: "Jan 25",
+        priority: "Normal"
+      });
+      this.taskValue = null;
+    }
+  }
+  // Cancel Task In Progress
+  onCancelTaskInProgress() {
+    this.isVisibleInProgress = !this.isVisibleInProgress;
+    this.taskValue = null;
+  }
+  // Add Task To Done list
+  isVisibleDone: boolean = false;
+  onAddTaskThree() {
+    this.isVisibleDone = !this.isVisibleDone;
+    console.log("Add Task Done", this.taskValue);
+  }
+  onAddTaskDone() { 
+    this.isVisibleDone = !this.isVisibleDone;
+    console.log("Add Task Done", this.taskValue);
+    if (this.taskValue != null) {
+      this.tasks.push({
+        id: this.tasks.length + 1,
+        title: this.taskValue,
+        description: "New Task",
+        status: "Done",
+        tags: [],
+        dueDate: "Jan 25",
+        priority: "Normal"
+      });
+      this.taskValue = null;
+    }
+  }
+  // Cancel Task Done 
+  onCancelTaskDone() {
+    this.isVisibleDone = !this.isVisibleDone;
+    this.taskValue = null;
+  }
 }
