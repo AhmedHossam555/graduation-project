09244078b736a32a3cb317861dfd4983dac07145
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-03 21:59:02 +0300

    update drag an drop method

--- src/app/pages/backlog/backlog.component.ts
+++ src/app/pages/backlog/backlog.component.ts
@@ -16,14 +16,12 @@ export class BacklogComponent {
     sprintTasks = [
       { title: 'Doggie','sprint-tasks':[  { title: 'Task 2', id: 1,status: 'To-do' }], id: 1, startDate: '2023-10-01', endDate: '2023-10-15' },
       { title: 'Fake','sprint-tasks':[  { title: 'Task 2', id: 2,status: 'To-do' }], id: 2, startDate: '2023-10-01', endDate: '2023-10-15' },
-
-
     ]
 
     backlogTasks:any[] = [
-      { title: 'Task 2', id: 1,status: 'To-do' },
+      { title: 'Task 1', id: 1,status: 'To-do' },
       { title: 'Task 2', id: 2,status: 'To-do' },
-      { title: 'Task 2', id: 3,status: 'To-do' },
+      { title: 'Task 3', id: 3,status: 'To-do' },
     ];
 
     currentItem: any = null;
@@ -72,11 +70,16 @@ export class BacklogComponent {
     
     onDrop(event: any, index: number) {
       event.preventDefault(); // Prevent default behavior
-      console.log("on Drop", index);
-      console.log("on Drop", event);
+      // console.log("on Drop", index);
+      // console.log("on Drop", event);
       if (!this.currentItem) return;
+      const record = this.backlogTasks.findIndex((m:any) => m.id == this.currentItem.id);
       if (index > -1) {
-        const task = this.backlogTasks.splice(index, 1)[0];
+        // console.log("on Drop", index);
+        // console.log("on Drop Index record", record);
+      
+        const task:any = this.backlogTasks.splice(record, 1)[0];
+        // console.log("on Drop task", task);
         this.sprintTasks[index]['sprint-tasks'].push(task); // Push to correct sprint
       }
     
