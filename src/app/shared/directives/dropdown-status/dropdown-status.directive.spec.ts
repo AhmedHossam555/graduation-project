24dc9ed4c4cfc1a9d6f98cc4ead2389dddcd74aa
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-04 00:46:00 +0300

    generate directive dropdown-status task

---
+++ src/app/shared/directives/dropdown-status/dropdown-status.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DropdownStatusDirective } from './dropdown-status.directive';
+
+describe('DropdownStatusDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DropdownStatusDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/shared/directives/dropdown-status/dropdown-status.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdownStatus]',
+  standalone: true
+})
+export class DropdownStatusDirective {
+
+  constructor() { }
+
+}
