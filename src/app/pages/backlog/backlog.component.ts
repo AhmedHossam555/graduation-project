c58b958e10c6aceda2b674cbe979540670154d6b
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-03 08:05:32 +0300

    addTaskToSprint method and addTaskToSprint Hnadler method

--- src/app/pages/backlog/backlog.component.ts
+++ src/app/pages/backlog/backlog.component.ts
@@ -9,33 +9,57 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './backlog.component.scss'
 })
 export class BacklogComponent {
+    // sprintTasks = [
+    //   { title: 'Task 2', id: 'DSP-1' }
+    // ];
     sprintTasks = [
-      { title: 'Task 2', id: 'DSP-1' }
-    ];
+      { title: 'Doggie','sprint-tasks':[  { title: 'Task 2', id: 'DSP-2',status: 'To-do' }], id: 'DSP-1' }
+    ]
 
     backlogTasks = [
       { title: 'Task 2', id: 'DSP-2',status: 'To-do' },
       { title: 'Task 2', id: 'DSP-2',status: 'To-do' },
       { title: 'Task 2', id: 'DSP-2',status: 'To-do' },
     ];
+
     currentItem: any = null;
     
 
 
 
-
-
     /// Add task to backlog
     isVisibleCreateIssue:boolean =false;
-    backlogVlaue: any ;
-    element?:HTMLElement;
+    backlogVlaue: string = '';
+
     addTaskToBacklog() {
       this.isVisibleCreateIssue = !this.isVisibleCreateIssue;
     }
     addTaskToBacklogHandler(event:any) {
+      console.log(this.backlogVlaue);
       if(event.key === 'Enter') {
-        this.backlogTasks.push({ title: this.backlogVlaue, id: 'DSP-2', status: 'To-do' });
-        this.backlogVlaue = '';
+        if(this.backlogVlaue.match(/[0-9a-zA-Z]{1,100}/)) {
+          this.backlogTasks.push({ title: this.backlogVlaue, id: 'DSP-2', status: 'To-do' });
+          this.backlogVlaue = '';
+          this.isVisibleCreateIssue = false;
+        }
+        this.isVisibleCreateIssue = false;
+      }
+    }
+
+    // Add task to sprint
+    isVisibleCreateSprintIssue:boolean =false;
+    sprintValue: string = '';
+    addTaskToSprint() {
+      this.isVisibleCreateIssue = !this.isVisibleCreateIssue;
+    }
+    addTaskToSprintHandler(event:any) {
+      console.log(this.backlogVlaue);
+      if(event.key === 'Enter') {
+        if(this.backlogVlaue.match(/[0-9a-zA-Z]{1,100}/)) {
+          this.backlogTasks.push({ title: this.backlogVlaue, id: 'DSP-2', status: 'To-do' });
+          this.backlogVlaue = '';
+          this.isVisibleCreateIssue = false;
+        }
         this.isVisibleCreateIssue = false;
       }
     }
