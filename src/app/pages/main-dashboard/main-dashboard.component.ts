d3a2e94f27b0a55dd6be825e203f2d7748508053
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-04 20:11:46 +0300

    footer ui design

--- src/app/pages/main-dashboard/main-dashboard.component.ts
+++ src/app/pages/main-dashboard/main-dashboard.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import {  RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "../../shared/layout/sidebar/sidebar.component";
 import { HeaderComponent } from "../../shared/layout/header/header.component";
+import { FooterComponent } from "../../shared/layout/footer/footer.component";
 
 @Component({
   selector: 'app-main-dashboard',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent, FooterComponent],
   templateUrl: './main-dashboard.component.html',
   styleUrl: './main-dashboard.component.scss'
 })
 export class MainDashboardComponent {
+  
 
 }
