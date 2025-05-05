1982f08bee5dc77234c52b7ae1b2b701af4f33de
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-06 02:48:01 +0300

    generate component add people modal

---
+++ src/app/shared/components/ui/add-people-modal/add-people-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-people-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-people-modal.component.html',
+  styleUrl: './add-people-modal.component.scss'
+})
+export class AddPeopleModalComponent {
+
+}
