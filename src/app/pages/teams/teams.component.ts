ca6fb94260dc624bfe4ace560162491f2559d008
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-06 02:53:02 +0300

    add people modal ui

--- src/app/pages/teams/teams.component.ts
+++ src/app/pages/teams/teams.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FlowbiteService } from '../../shared/services/flowbite/flowbite.service';
 import { initFlowbite } from 'flowbite';
+import { AddPeopleModalComponent } from "../../shared/components/ui/add-people-modal/add-people-modal.component";
 
 @Component({
   selector: 'app-teams',
   standalone: true,
-  imports: [],
+  imports: [AddPeopleModalComponent],
   templateUrl: './teams.component.html',
   styleUrl: './teams.component.scss'
 })
