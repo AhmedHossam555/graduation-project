503d8bc96dc93067bdcd7ea122b8a9323d686cfe
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-06 06:11:53 +0300

    update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,13 +9,13 @@ export const routes: Routes = [
     {path:'projects', loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent)}, 
     {path:'teams', loadComponent: ()=> import('./pages/teams/teams.component').then(m => m.TeamsComponent)}
   ]},
-  {path:'main-dashboard', loadComponent: () => import('./pages/main-dashboard/main-dashboard.component').then(m => m.MainDashboardComponent), children:[
-    {path:'', redirectTo:'dashboard', pathMatch:'full'},
-    {path:'dashboard', loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)},
-    {path:'task',loadComponent: () => import('./pages/task/task.component').then(m => m.TaskComponent)},
-    {path:'chat', loadComponent: () => import('./pages/chat/chat.component').then(m => m.ChatComponent)}, 
-    {path:'notification', loadComponent: () => import('./pages/notification/notification.component').then(m => m.NotificationComponent)},
+  {path:'projects/SCRUM/boards/:id',loadComponent:()=> import('./pages/boards/boards.component').then(m => m.BoardsComponent),children:[
+    {path:'', redirectTo:'backlog', pathMatch:'full'},
     {path:'backlog', loadComponent: () => import('./pages/backlog/backlog.component').then(m => m.BacklogComponent)},
     {path:'active-sprint', loadComponent: () => import('./pages/active-sprint/active-sprint.component').then(m => m.ActiveSprintComponent)},
-  ] }
+    {path:'summary', loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)},
+    {path:'chat', loadComponent: () => import('./pages/chat/chat.component').then(m => m.ChatComponent)}, 
+    {path:'notification', loadComponent: () => import('./pages/notification/notification.component').then(m => m.NotificationComponent)},
+  ]},
+
 ];
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FlowbiteService } from '../../shared/services/flowbite/flowbite.service';
 import { initFlowbite } from 'flowbite';
 import { ProjectModalComponent } from '../../shared/components/ui/project-modal/project-modal.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [ProjectModalComponent],
+  imports: [ProjectModalComponent,RouterLink],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
