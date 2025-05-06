7822bf060340ed247620a5af90c97148fea075ff
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-06 06:01:23 +0300

    sidebar  html and tailwind code

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/layout/header/header.component";
-import { MainSidebarComponent } from "../../shared/layout/main-sidebar/main-sidebar.component";
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "../../shared/layout/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [HeaderComponent, MainSidebarComponent,RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, SidebarComponent],
   templateUrl: './boards.component.html',
   styleUrl: './boards.component.scss'
 })
