89ba77bfe4b6c954eadae03824bd171d50a5031a
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-06 06:52:20 +0300

    add input decorator to accordion directive

--- src/app/shared/directives/accordion/accordion.directive.ts
+++ src/app/shared/directives/accordion/accordion.directive.ts
@@ -1,11 +1,14 @@
-import { Directive } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Directive, ElementRef, Inject, Input } from '@angular/core';
 
 @Directive({
   selector: '[appAccordion]',
   standalone: true
 })
 export class AccordionDirective {
+  @Input('appAccordion') bodyId?:string;
+  constructor(private _elementRef:ElementRef,@Inject(DOCUMENT) private document:Document ) { 
+  }
 
-  constructor() { }
 
 }
