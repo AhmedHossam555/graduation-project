2dbcb2c70862ef77530228d1f842b09a00a7c257
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-06 06:55:54 +0300

    hostlistener decorator event => click

--- src/app/shared/directives/accordion/accordion.directive.ts
+++ src/app/shared/directives/accordion/accordion.directive.ts
@@ -1,14 +1,23 @@
 import { DOCUMENT } from '@angular/common';
-import { Directive, ElementRef, Inject, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Inject, Input } from '@angular/core';
 
 @Directive({
   selector: '[appAccordion]',
   standalone: true
 })
 export class AccordionDirective {
+  // Input decorator  bodyId
   @Input('appAccordion') bodyId?:string;
   constructor(private _elementRef:ElementRef,@Inject(DOCUMENT) private document:Document ) { 
   }
+  
+  // hostlistener decorator event => click
+  @HostListener('click') onClick(){
+    this._elementRef.nativeElement.classList.toggle('rotate-90');
+    if(this.bodyId)
+    this.document.getElementById(this.bodyId)?.classList.toggle('hidden');
+  }
+
 
 
 }
