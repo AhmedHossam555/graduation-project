d29131415799ddd8db1d15d9854510c3c02235e6
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-06 03:44:21 +0300

    create team modal component and ui design for your

---
+++ src/app/shared/components/ui/create-team-modal/create-team-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-team-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-team-modal.component.html',
+  styleUrl: './create-team-modal.component.scss'
+})
+export class CreateTeamModalComponent {
+
+}
