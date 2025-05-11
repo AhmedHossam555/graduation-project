4a690bf38ffc6cfdd24ad472793de886cdd531f8
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-12 01:30:50 +0300

    Add bar chart component desgin

--- src/app/pages/dashboard/components/chart-summary/components/bar/bar.component.ts
+++ src/app/pages/dashboard/components/chart-summary/components/bar/bar.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-bar',
   standalone: true,
-  imports: [],
+  imports: [ChartModule],
   templateUrl: './bar.component.html',
   styleUrl: './bar.component.scss'
 })
 export class BarComponent {
+priorityData = {
+  labels: ['Highest', 'High', 'Medium', 'Low', 'Lowest'],
+  datasets: [
+    {
+      label: 'Tasks',
+      backgroundColor: '#9CA3AF',
+      data: [0, 0, 4, 0, 0]
+    }
+  ]
+};
+
+priorityOptions = {
+  plugins: {
+    legend: {
+      display: false
+    }
+  },
+  scales: {
+    y: {
+      beginAtZero: true,
+      ticks: {
+        stepSize: 1
+      }
+    }
+  }
+};
 
 }
