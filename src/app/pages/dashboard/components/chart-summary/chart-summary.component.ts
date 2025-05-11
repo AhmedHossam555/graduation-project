d8573d5b2c504c4b4bc5970e5869c55b2bfb8c88
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-12 01:16:58 +0300

    add app-bar to cahrt-summary

--- src/app/pages/dashboard/components/chart-summary/chart-summary.component.ts
+++ src/app/pages/dashboard/components/chart-summary/chart-summary.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 
 import { ChartModule } from 'primeng/chart';
 import { DoughnutComponent } from "./components/doughnut/doughnut.component";
+import { BarComponent } from "./components/bar/bar.component";
 
 @Component({
   selector: 'app-chart-summary',
   standalone: true,
-  imports: [ChartModule, DoughnutComponent],
+  imports: [ChartModule, DoughnutComponent, BarComponent],
   templateUrl: './chart-summary.component.html',
   styleUrl: './chart-summary.component.scss'
 })
