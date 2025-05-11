54d554fb726fa3ed41cd58d1d24bd56bc6757228
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-11 16:37:43 +0300

    Doughnut chart

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,10 +3,11 @@ import { MenuItem } from 'primeng/api';
 import { MenuModule } from 'primeng/menu';
 import { ButtonModule } from 'primeng/button';
 import { HeaderSummaryComponent } from "./components/header-summary/header-summary.component";
+import { ChartSummaryComponent } from "./components/chart-summary/chart-summary.component";
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MenuModule, ButtonModule, HeaderSummaryComponent],
+  imports: [MenuModule, ButtonModule, HeaderSummaryComponent, ChartSummaryComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
