5eee020925f917c4f790281829c0fed095c070a7
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-11 08:41:42 +0300

    provide animation in app.config

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MenuModule, ButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+    items: MenuItem[] | undefined;
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Options',
+                items: [
+                    {
+                        label: 'Refresh',
+                        icon: 'pi pi-refresh'
+                    },
+                    {
+                        label: 'Export',
+                        icon: 'pi pi-upload'
+                    }
+                ]
+            }
+        ];
+    }
 
 }
