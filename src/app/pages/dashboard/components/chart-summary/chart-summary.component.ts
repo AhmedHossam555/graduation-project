5e4540f769d94a1816df6edcbee0c5e3d9fba93b
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-12 02:29:51 +0300

    add bar team work component in summary chart

--- src/app/pages/dashboard/components/chart-summary/chart-summary.component.ts
+++ src/app/pages/dashboard/components/chart-summary/chart-summary.component.ts
@@ -5,11 +5,12 @@ import { ChartModule } from 'primeng/chart';
 import { DoughnutComponent } from "./components/doughnut/doughnut.component";
 import { BarComponent } from "./components/bar/bar.component";
 import { HorizontalBarComponent } from "./components/horizontal-bar/horizontal-bar.component";
+import { BarTeamWorkComponent } from "./components/bar-team-work/bar-team-work.component";
 
 @Component({
   selector: 'app-chart-summary',
   standalone: true,
-  imports: [ChartModule, DoughnutComponent, BarComponent, HorizontalBarComponent],
+  imports: [ChartModule, DoughnutComponent, BarComponent, HorizontalBarComponent, BarTeamWorkComponent],
   templateUrl: './chart-summary.component.html',
   styleUrl: './chart-summary.component.scss'
 })
