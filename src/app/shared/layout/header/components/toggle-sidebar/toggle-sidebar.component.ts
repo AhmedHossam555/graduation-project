83c69530a96851845b01a04470fe3a5d9df6a953
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-12 05:26:02 +0300

    generate toggle-sidebar

---
+++ src/app/shared/layout/header/components/toggle-sidebar/toggle-sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './toggle-sidebar.component.html',
+  styleUrl: './toggle-sidebar.component.scss'
+})
+export class ToggleSidebarComponent {
+
+}
