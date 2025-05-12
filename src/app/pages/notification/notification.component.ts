aaad537c5713f51fcce9efd1c9f3fa6c7cb37c61
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-12 03:47:00 +0300

    notification page ui

--- src/app/pages/notification/notification.component.ts
+++ src/app/pages/notification/notification.component.ts
@@ -1,12 +1,45 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgClass, NgIf],
   templateUrl: './notification.component.html',
   styleUrl: './notification.component.scss'
 })
 export class NotificationComponent {
-
+ groupedNotifications = [
+    {
+      date: 'Today',
+      items: [
+        {
+          title: 'Task Assigned',
+          message: 'You were assigned to "Bug Fix #234"',
+          time: '2 hours ago',
+          icon: 'pi pi-check-circle',
+          read: false
+        },
+        {
+          title: 'New Comment',
+          message: 'Jane commented on "Sprint Planning"',
+          time: '3 hours ago',
+          icon: 'pi pi-comment',
+          read: true
+        }
+      ]
+    },
+    {
+      date: 'Yesterday',
+      items: [
+        {
+          title: 'Issue Resolved',
+          message: 'The issue "Login not working" was marked resolved.',
+          time: '1 day ago',
+          icon: 'pi pi-check',
+          read: true
+        }
+      ]
+    }
+  ];
 }
