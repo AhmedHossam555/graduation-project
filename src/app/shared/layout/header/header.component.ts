23ad0c6d8dc2abaeda6984774dfd0c186c2848a4
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-12 05:50:21 +0300

    add toggle sidebar from flowbite service

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
+import { ToggleSidebarComponent } from "./components/toggle-sidebar/toggle-sidebar.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, SidebarModule, ButtonModule, ToggleSidebarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+    sidebarVisible: boolean = false;
 }
