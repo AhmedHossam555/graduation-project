726392452c6db3841363bb71ab6f1175e94efa4f
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-13 07:20:46 +0300

    add zommin  and zoom out method

--- src/app/shared/components/business/workflow/workflow.component.ts
+++ src/app/shared/components/business/workflow/workflow.component.ts
@@ -1,18 +1,51 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SliderModule } from 'primeng/slider';
 
 @Component({
   selector: 'app-workflow',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,SliderModule],
   templateUrl: './workflow.component.html',
   styleUrl: './workflow.component.scss'
 })
 export class WorkflowComponent {
   activeStroke:string ='#0C66E4';
+  zoomValue:number = 0;
   workflow:any[] = [
-  { id: 'todo', label: 'TO DO', transitions: ['inprogress', 'done'] },
-];;
+    { id: 'todo', label: 'TO DO', transitions: ['inprogress', 'done'] },
+  ];
+  
+  // // method for zoom In
+  // zoomIn(){
+  //   this.zoomValue += 5;
+  // }
+
+  // // method for zoom out
+  // zoomOut(){
+  //   this.zoomValue -= 5;
+
+  // }
+
+zoomLevel = 1;
+minZoom = 0.1;
+maxZoom = 2;
+
+zoomIn() {
+  if (this.zoomLevel < this.maxZoom) {
+    this.zoomLevel = Math.round((this.zoomLevel + 0.1) * 10) / 10;
+  }
+}
+
+zoomOut() {
+  if (this.zoomLevel > this.minZoom) {
+    this.zoomLevel = Math.round((this.zoomLevel - 0.1) * 10) / 10;
+  }
+}
+  
+
+
  
 }
 
