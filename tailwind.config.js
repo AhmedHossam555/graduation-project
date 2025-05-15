e2a81257a8bccecfeea74c67bea883b6daf7e7dd
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-15 10:53:12 +0300

    add custome dark color in tailiwnd.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,24 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
+
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'black':{
+          '50':'#90969B',
+          '100':'#1D2328 ',
+          '200':'#15191C'
+        }
+      }
+    }
   },
-  plugins: [],
-  darkMode: 'class',
+  plugins: [
+      require('flowbite/plugin')
+  ],
+  darkMode: 'class',// enables dark mode using the 'dark' class 
 }
+
+
