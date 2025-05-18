15a5ba7fd49f4114409df1494f1bb74f5b3b52a1
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-05-18 11:50:58 +0300

    generate interface projects

---
+++ src/app/shared/interfaces/project.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  name: string;
+  description?: string;
+  end_date?: string;
+  id: number;
+  created_at?: string;
+  updated_at?: string;
+  deleted_at?: string;
+  start_date?: string;
+}
