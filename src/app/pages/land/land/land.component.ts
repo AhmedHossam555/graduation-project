b60eaa2f9640bfb449b181b669874ca2e3cbc4b5
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-06-09 01:17:02 +0300

    hero section style

--- src/app/pages/land/land/land.component.ts
+++ src/app/pages/land/land/land.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { HeroComponent } from "../hero/hero.component";
 
 @Component({
   selector: 'app-land',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, HeroComponent],
   templateUrl: './land.component.html',
   styleUrl: './land.component.scss'
 })
