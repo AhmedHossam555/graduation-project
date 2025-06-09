3f37c0adc3c798003cf19c39d223fa1329ce5ce2
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-06-09 15:37:28 +0300

    add activateroute for toggle sidebar

--- src/app/shared/directives/scroll/scroll.directive.ts
+++ src/app/shared/directives/scroll/scroll.directive.ts
@@ -6,18 +6,18 @@ import { Directive, ElementRef, Host, HostListener, Inject } from '@angular/core
   standalone: true
 })
 export class ScrollDirective {
-
   constructor(private _ele:ElementRef,@Inject(DOCUMENT) private document:Document) { }
 
+  // This directive listens to the scroll event and hides the element when the user scrolls to the bottom of the page
+  // and shows it when the user scrolls up.
+  // It is useful for hiding elements like a "Back to Top" button or a floating action button when the user is at the bottom of the page.
   @HostListener('window:scroll', ['$event']) onScroll(){
-     const scrollTop = this.document.documentElement.scrollTop || this.document.body.scrollTop;
-     const footer = this.document.querySelector('#footer');
-
+    const scrollTop = this.document.documentElement.scrollTop || this.document.body.scrollTop;
+    const footer = this.document.querySelector('#footer');
     if (footer && typeof footer.scrollHeight === 'number' && scrollTop >= footer.scrollHeight - 50 ) {
       this._ele.nativeElement.style.display = 'none';
     }else{
       this._ele.nativeElement.style.display = 'flex';
     }
-
   }
 }
--- src/app/shared/layout/header/components/toggle-sidebar/toggle-sidebar.component.ts
+++ src/app/shared/layout/header/components/toggle-sidebar/toggle-sidebar.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-toggle-sidebar',
@@ -9,5 +9,18 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './toggle-sidebar.component.scss'
 })
 export class ToggleSidebarComponent {
+
+  isHome = signal(false);
+
+  constructor(private router:ActivatedRoute){
+    this.router.url.subscribe(url => {
+      if(url[0].path === 'home'){
+        this.isHome.set(true);
+      }else{
+        this.isHome.set(false); 
+      }
+    });
+  }
+
 
 }
