b1a6c1fd1f222d24b75713d55fc7f7bc36c8d571
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-06-11 11:50:24 +0300

    add routing in header land

--- src/app/pages/land/header/header.component.ts
+++ src/app/pages/land/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from '../../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isLogin = signal<boolean>(false);
+  private auth = inject(AuthService);
+  ngOnInit() {
+
+    this.auth.userData.subscribe((res)=>{
+      if(res){
+        this.isLogin.set(true);
+      }else{
+        this.isLogin.set(false);
+      }
+    })
+  
+
+  }
 
 }
